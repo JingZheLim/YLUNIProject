b222757a949aabb0d1cb30d68ba7da351f181ad2
Lucas <jingzhe.lim@student.adelaide.edu.au>
2023-10-14 19:25:42 +1030

    Completed player's element introduction

--- game.h
+++ game.h
@@ -19,14 +19,14 @@ class game : menu {
 
     // when run the menu will first be displayed
     // types number in terminal
-    // if 1 start the game
-    if (get_userChoice() == 1) {
+
+    if (get_userChoice() == 1) {  // if 1 start the game
+      system("clear");            // clears terminal
       menuElement();
       // move into different storylines for the different elements
-      if (get_userChoice() == 1) {
-        // Water
-        system("clear");  // clears terminal
-        cout << "                       GAME START                    " << endl;
+      if (get_userChoice() == 1) {  // Water
+        system("clear");
+        cout << "                      GAME START                     " << endl;
         cout << "-----------------------------------------------------" << endl;
         cout << "The water kingdom is going through a drought because of the "
                 "increasing temperatures."
@@ -43,20 +43,64 @@ class game : menu {
         cout << "What is your name brave mage?" << endl;
         cout << "My name is: " << endl;
         // cin << playerName;
-      } else if (get_userChoice() == 2) {
-        // Earth
-      } else if (get_userChoice() == 3) {
-        // Fire
-      } else if (get_userChoice() == 4) {
-        // Air
+      } else if (get_userChoice() == 2) {  // Earth
+        system("clear");
+        cout << "                      GAME START                     " << endl;
+        cout << "-----------------------------------------------------" << endl;
+        cout << "The earth kingdom is sick and tired of everyone fighting "
+                "because of the evil Dragon\'s powers and chaos it creates so "
+                "you must help your kingdom to defeat it and restore peace for "
+                "everyone."
+             << endl;
+        cout << "However you must also defeat the other elements\' monsters in "
+                "order to become powerful enough to take on the dragon."
+             << endl;
+        cout << "You are a wise earth elder. " << endl;
+        cout << "-----------------------------------------------------" << endl;
+        cout << "What is your name wise elder??" << endl;
+        cout << "My name is: " << endl;
+        // cin << playerName;
+      } else if (get_userChoice() == 3) {  // Fire
+        system("clear");
+        cout << "                      GAME START                     " << endl;
+        cout << "-----------------------------------------------------" << endl;
+        cout << "Your kingdom is hungry for power over the other kingdoms and "
+                "has set you on an adventure to defeat the almighty dragon so "
+                "you can learn other kingdoms\' spells."
+             << endl;
+        cout << "This will set your kingdom above the others." << endl;
+        cout << "You are a fire warrior." << endl;
+        cout << "-----------------------------------------------------" << endl;
+        cout << "What is your name warrior?" << endl;
+        cout << "My name is: " << endl;
+        // cin << playerName;
+      } else if (get_userChoice() == 4) {  // Air
+        system("clear");
+        cout << "                      GAME START                     " << endl;
+        cout << "-----------------------------------------------------" << endl;
+        cout << "You gained inside intel that the fire kingdom is trying to "
+                "gain power over other kingdoms by sending someone to defeat "
+                "the dragon."
+             << endl;
+        cout << "In order to prevent the fire kingdom from gaining too much "
+                "power and causing chaos themselves, you must defeat the "
+                "dragon yourself to protect everyone from the fire kingdom."
+             << endl;
+        cout << "You are a lone air adventurer." << endl;
+        cout << "-----------------------------------------------------" << endl;
+        cout << "What is your name adventurer?" << endl;
+        cout << "My name is: " << endl;
+        // cin << playerName;
       }
     }
-    // else if 2 use save file
-    else if (get_userChoice() == 2) {
+
+    else if (get_userChoice() == 2) {  // else if 2 use save file
+      system("clear");                 // clears terminal
       cout << "Does nothing for now" << endl;
     }
-    // else if 3 end the game
-    else if (get_userChoice() == 3) {
+
+    else if (get_userChoice() == 3) {  // else if 3 end the game
+      system("clear");                 // clears terminal
       cout << "Thanks for Playing!!" << endl;
       return;
     }
